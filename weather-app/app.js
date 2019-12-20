9c0ee6ded3129ad98baee51280993bff418b2970
v-vova-v <vmaka@softserveinc.com>
2019-12-20 20:29:43 +0200

    weather-app: configure express.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,32 @@
 const geolocationService = require('./services/geolocation.service');
 const forecastService = require('./services/forecast.service');
+const express = require('express');
+const app = express();
 
-geolocationService.getLocationData('Rivne', (error, response) => {
+let forecast;
+
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', '*');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type, authorization');
+  next();
+});
+
+app.get('/forecast', (req, res) => {
+  res.send(forecast);
+});
+
+geolocationService.getLocationData('New York', (error, response) => {
   if (response && response.features) {
     forecastService.getLocationWeather(response.features[0].center, (error, response) => {
       if (response) {
-        console.log(response.hourly);
+        forecast = response.currently;
       }
     });
   }
+});
+
+
+app.listen(3000, () => {
+  console.log('Strating server on port 3000');
 });
